c8469c3d57586cbd96862fb2cdaeef2cf0ed19e7
Dhruv Kar <kar.dhruv@gmail.com>
2013-11-13 21:57:10 -0600

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name        "Dhruv Kar"
+    email       "kar.dhruv@gmail.com"
+    password    "foobar"
+    password_confirmation "foobar"
+  end
+end
